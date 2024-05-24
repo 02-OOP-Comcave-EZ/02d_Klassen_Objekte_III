beabe318f0277974164bcbb3f838c95c3f2ab8fb
Esther <esther.zirkler@gmail.com>
2024-05-24 11:53:37 +0200

    02 / checkCompliance() + switch - case

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -6,6 +6,7 @@ public class Cat {
   private String furColor;
   private int age; 
   private boolean isFemale;
+  private int counter;
 
 
 
@@ -35,11 +36,29 @@ public String getStringAttributes(String op) {
 
 public String getAge() {
   if (isFemale) {
-    return "This is an inappropriate question!";
+    return checkCompliance();
   } else {
     return String.valueOf(age);
   }
     
+}
+
+  private String checkCompliance(){
+    
+   counter++;
+    
+    switch (counter) {
+      case 1:
+        return "This is an inappropriate question!";
+      case 2:
+       return "I've told you once!";
+      case 3:
+        return "Talk to the hand!";
+      default:
+        return "1#!?&&%";
+    
+    }
+ 
   }
 }
 
