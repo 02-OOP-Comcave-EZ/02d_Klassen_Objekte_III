be0965b852d763bd121291441828b4ef099ebb2d
Esther <esther.zirkler@gmail.com>
2024-05-24 10:52:41 +0200

    01 / if-else + boolean isFemale

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,13 +5,22 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
+  private boolean isFemale;
 
 
-public Cat(String name, String furColor, int age) {
+
+public Cat(String name, String furColor, int age, boolean isFemale) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
+    this.isFemale = isFemale;
   }
+
+// public Cat(String name, String furColor, int age) {
+//     this.name = name;
+//     this.furColor = furColor;
+//     this.age = age;
+//   }
 
 public String getStringAttributes(String op) {
   switch (op) {
@@ -25,9 +34,15 @@ public String getStringAttributes(String op) {
 }
 
 public String getAge() {
+  if (isFemale) {
+    return "This is an inappropriate question!";
+  } else {
     return String.valueOf(age);
   }
-
-
+    
+  }
 }
+
+
+
   
